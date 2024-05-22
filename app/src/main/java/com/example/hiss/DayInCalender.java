6ef235b16f1970015d985051129c4c67bf3ec8d1
OctopusEmperor <ivrilevi@gmail.com>
2024-05-23 02:52:40 +0300

    Working on day in calendar

---
+++ app/src/main/java/com/example/hiss/DayInCalender.java
@@ -0,0 +1,14 @@
+package com.example.hiss;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DayInCalender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
